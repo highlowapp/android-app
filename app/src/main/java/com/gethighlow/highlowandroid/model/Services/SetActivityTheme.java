00997ead8d6520bd2fe542cb12d59bf28191fa55
Brayden <bmorris2405@gmail.com>
2020-05-04 16:40:25 -0700

    Add change dark theme on system ui change

--- app/src/main/java/com/gethighlow/highlowandroid/model/Services/SetActivityTheme.java
+++ app/src/main/java/com/gethighlow/highlowandroid/model/Services/SetActivityTheme.java
@@ -1,24 +1,41 @@
 package com.gethighlow.highlowandroid.model.Services;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.preference.PreferenceManager;
 
 import com.gethighlow.highlowandroid.R;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class SetActivityTheme {
     public static String getTheme(Context context){
+        int currentNightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
 
         String currentTheme = sharedPref.getString("current-theme", "light");
 
-        if (currentTheme.equals("light"))
+        if (currentTheme.equals("light")) {
 
             return "light";
-        else if(currentTheme.equals("dark"))
+
+        }else if(currentTheme.equals("dark")) {
+
             return "dark";
+
+        }else if(currentTheme.equals("auto")) {
+
+            if (currentNightMode == Configuration.UI_MODE_NIGHT_NO) {
+                return "light";
+            } else if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+                return "dark";
+            }
+        }
         return currentTheme;
+
     }
 
     public static void setTheme(Context context){
@@ -27,8 +44,10 @@ public class SetActivityTheme {
             context.setTheme(R.style.LightTheme);
         } else if(theme.equals("dark")){
             context.setTheme(R.style.DarkTheme);
+        } /*else if(theme.equals("auto")){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         }
-
+*/
     }
 
 }
